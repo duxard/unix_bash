e49f84fcc22a34d1250173fb455f52390d8d563f
Aleksandr <duxard@gmail.com>
2021-01-12 02:57:00 -0500

    Update useful.sh

--- useful.sh
+++ useful.sh
@@ -1,3 +1,9 @@
+# the amount of free memory
+free -m
+
+# disk space usage
+df -kh
+
 # get list of deleted files which have open descriptors
 lsof -nP +L1 | grep deleted
 
@@ -19,5 +25,11 @@ find [path] -size +1G | xargs du -sh | sort
 # find all files with a certain name and delete those (useful when cleaning old logs)
 find [path] -name "*.log.2020*" -delete
 
+#search for the files containing "stao" string in all folders & subfolders
+find . -type f | xargs grep -l 'stao'
 
+# grep for a 'pattern' only in '*.d.ts' files along the whole project and output file's names only:
+grep -rl --include=\*.d.ts 'pattern' ./*
 
+# grep for a 'pattern' only in '*.d.ts' files in particular folders ('ag-grid-...') along the whole project and output file's names only:
+grep -rl --include=\*.d.ts 'pattern' ./*a-grid*/*
